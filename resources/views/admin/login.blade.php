45b6df3f073d1b6376eafd8d4369f921274c7010
deformator852 <kostneko30@gmail.com>
2025-05-20 04:34:35 -0400

    changes

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts.app')
 
+
 @section('title',$title)
 @section('content')
-    <div class="login">
-        <form class="login__form" action="" method="POST">
-            <div class="errors">
-                @if ($errors->any())
-                    @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
-                    @endforeach
-                @endif
+		<div class="login">
+				<form class="login__form" action="" method="POST">
+						<div class="errors">
+								@if ($errors->any())
+										@foreach ($errors->all() as $error)
+												<p>{{ $error }}</p>
+										@endforeach
+								@endif
 
-            </div>
-            @csrf
-            <div>
-                <input class="input"
-                       type="text" name="name" id="name" placeholder="username" required>
-            </div>
-            <div>
-                <input class="input"
-                       type="password" name="password" id="password" placeholder="**************" required>
-            </div>
-            <div>
-                <button class="login__submit submit" type="submit">login</button>
-            </div>
-        </form>
-    </div>
+						</div>
+						@csrf
+						<div>
+								<input class="input"
+								       type="text" name="name" id="name" placeholder="username" required>
+						</div>
+						<div>
+								<input class="input"
+								       type="password" name="password" id="password" placeholder="**************" required>
+						</div>
+						<div>
+								<button class="login__submit submit" type="submit">login</button>
+						</div>
+				</form>
+		</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,19 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('admin')->group(function () {
-    Route::view('/login', 'admin.login', ['title' => 'Login']);
-    Route::post('/login', [AdminController::class, 'login']);
-    Route::get('/panel', [AdminController::class, 'panel'])->middleware('admin')->name('admin.panel');
-});
+use App\Http\Controllers\ProductController;
 
 Route::prefix('products')->group(function () {
-    Route::get('/', [ProductController::class, 'list'])->name('products.list');
-    Route::post('/', [ProductController::class, 'store'])->middleware('admin')->name('products.store');
-    Route::delete('/{product}', [ProductController::class, 'destroy'])->middleware('admin')->name('products.destroy');
+	Route::get('/', [ProductController::class, 'list'])->name('products.list');
+	Route::post('/', [ProductController::class, 'store'])->middleware('admin')->name('products.store');
+	Route::delete('/{product}', [ProductController::class, 'destroy'])->middleware('admin')->name('products.destroy');
+});
+
+Route::prefix('admin')->group(function () {
+	Route::view('/login', 'admin.login', ['title' => 'Login']);
+	Route::post('/login', [AdminController::class, 'login']);
+	Route::get('/panel', [AdminController::class, 'panel'])->middleware('admin')->name('admin.panel');
 });
 
 
